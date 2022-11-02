d7f929ecc707faefa9274a3136a0bd6bf50b63ae
Oleksii-onufriienko <110289186+Oleksii-onufriienko@users.noreply.github.com>
2022-11-02 10:38:33 +0000

    Deploying to gh-pages from @ Oleksii-onufriienko/goit-js-hw-11@de0b8fc0e01552bf3c8540b37228e85da996ded5 🚀

--- index.31339e7f.js
+++
@@ -1,2 +0,0 @@
-const e={form:document.querySelector("#search-form"),gallery:document.querySelector("gallery")};console.log(e),e.form.addEventListener("submit",(function(e){const r=e.currentTarget.searchQuery.value.trim();e.preventDefault(),console.log(r)}));
-//# sourceMappingURL=index.31339e7f.js.map
--- index.c220bc7a.js
+++
@@ -1,2 +0,0 @@
-!function(){var e={form:document.querySelector("#search-form"),gallery:document.querySelector("gallery")};console.log(e),e.form.addEventListener("submit",(function(e){var r=e.currentTarget.searchQuery.value.trim();e.preventDefault(),console.log(r)}))}();
-//# sourceMappingURL=index.c220bc7a.js.map
